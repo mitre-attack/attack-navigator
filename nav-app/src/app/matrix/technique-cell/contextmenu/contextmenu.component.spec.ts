fe69aed4f174bac26a5e9795c11e2812ee68dd03
Isabel Tuson <isabel@mitre.org>
2020-12-07 15:01:21 -0500

    upgrade to Angular 11

--- nav-app/src/app/matrix/technique-cell/contextmenu/contextmenu.component.spec.ts
+++ nav-app/src/app/matrix/technique-cell/contextmenu/contextmenu.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ContextmenuComponent } from './contextmenu.component';
 
@@ -6,7 +6,7 @@ describe('ContextmenuComponent', () => {
   let component: ContextmenuComponent;
   let fixture: ComponentFixture<ContextmenuComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ContextmenuComponent ]
     })
