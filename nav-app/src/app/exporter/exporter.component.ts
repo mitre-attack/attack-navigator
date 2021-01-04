497edb0fdfe6cbf7888e64dadb0ad14aeb110cbc
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2021-01-04 12:15:12 -0500

    fix for dialog imports

--- nav-app/src/app/exporter/exporter.component.ts
+++ nav-app/src/app/exporter/exporter.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ViewModel, TechniqueVM } from "../viewmodels.service";
 import { ConfigService } from "../config.service";
 import { Technique, DataService, Tactic, Matrix } from '../data.service';
