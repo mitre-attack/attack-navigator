302de644de66355a1d5d4b67fac8d6b663a3f60e
Anna Lin <alin@mitre.org>
2021-03-11 17:33:09 -0800

    Added min, max, and sum functions

--- nav-app/src/app/viewmodels.service.ts
+++ nav-app/src/app/viewmodels.service.ts
@@ -979,9 +979,9 @@ export class ViewModel {
   }
 
   public calculateAggregateScore(techniqueScore: number, technique: Technique, tactic: Tactic): number {
-    // TODO, add toggle feature to count unscored subtechniques in aggScore division
     const tvm = this.getTechniqueVM(technique, tactic);
     let score = techniqueScore;
+    const scores = [techniqueScore];
     let validSubTechniquesCount = (techniqueScore > 0) ? 1 : 0;
     technique.subtechniques.forEach((subtechnique) => {
       const techniqueVM = this.getTechniqueVM(subtechnique, tactic);
@@ -989,11 +989,27 @@ export class ViewModel {
         validSubTechniquesCount += 1;
       }
       score += techniqueVM.score.length > 0 ? Number(techniqueVM.score) : 0;
+      scores.push(+techniqueVM.score);
     });
-
-    // Divide by count of all subtechniques + 1 (for parent technique) if counting unscored is enabled
-    // Otherwise, divide by count of all scored only
-    const aggScore = +(score / ((this.layout.countUnscored) ? technique.subtechniques.length + 1 : validSubTechniquesCount)).toFixed(2);
+    let aggScore = 0;
+    switch (this.layout.aggregateFunction) {
+      default:
+      case "avg":
+        // Divide by count of all subtechniques + 1 (for parent technique) if counting unscored is enabled
+        // Otherwise, divide by count of all scored only
+        aggScore = +(score / ((this.layout.countUnscored) ? technique.subtechniques.length + 1 : validSubTechniquesCount)).toFixed(2);
+        break;
+      case "min":
+        const reducer = (min, currentValue) =>  min < currentValue && min > 0 ? min : currentValue;
+        aggScore = (this.layout.countUnscored) ? Math.min(...scores) : scores.reduce(reducer);
+        break;
+      case "max":
+        aggScore = Math.max(...scores);
+        break;
+      case "sum":
+        aggScore = score;
+        break;
+    }
     tvm.aggregateScoreColor = this.gradient.getColor(aggScore.toString());
     return aggScore;
   }
