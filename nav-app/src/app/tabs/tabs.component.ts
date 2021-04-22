756d02c7283cea4d2920dd26c1b0a17dc01043ee
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2021-04-22 15:46:36 -0400

    compare stix bundle versions when user upgrades layer version on import

--- nav-app/src/app/tabs/tabs.component.ts
+++ nav-app/src/app/tabs/tabs.component.ts
@@ -3,6 +3,7 @@ import { Component, AfterContentInit, ViewChild, TemplateRef, AfterViewInit } fr
 import { DataService, Technique } from '../data.service'; //import the DataService component so we can use it
 import { ConfigService } from '../config.service';
 import * as is from 'is_js';
+import { forkJoin } from 'rxjs';
 import { VersionUpgradeComponent } from '../version-upgrade/version-upgrade.component';
 import { HelpComponent } from '../help/help.component';
 import { ExporterComponent } from '../exporter/exporter.component';
@@ -487,8 +488,10 @@ export class TabsComponent implements AfterContentInit, AfterViewInit {
                             this.alwaysUpgradeVersion = result.upgrade;
                         }
                         if (result.upgrade) {
+                            let prevDomainID = viewModel.domainID;
                             viewModel.version = currVersion
                             viewModel.domainID = this.dataService.getDomainID(viewModel.domain, viewModel.version);
+                            resolve({previous: prevDomainID, latest: viewModel.domainID});
                         }
                         resolve(null);
                     },
@@ -530,9 +533,26 @@ export class TabsComponent implements AfterContentInit, AfterViewInit {
                 if (!this.dataService.getDomain(viewModel.domainID)) {
                     throw {message: "Error: '" + viewModel.domain + "' (" + viewModel.version + ") is an invalid domain."};
                 }
-                this.versionUpgradeDialog(viewModel).then( () => {
+                this.versionUpgradeDialog(viewModel).then( (versions) => {
+                    //TODO: don't automatically transfer over the annotations
                     this.openTab("new layer", viewModel, true, true, true, true);
-                    if (!this.dataService.getDomain(viewModel.domainID).dataLoaded) {
+                    if (versions) { // user upgraded to latest version
+                        let loads: any = {};
+                        if (!this.dataService.getDomain(versions.previous).dataLoaded) loads.previous = this.dataService.loadDomainData(versions.previous, true, true);
+                        if (!this.dataService.getDomain(versions.latest).dataLoaded) loads.latest = this.dataService.loadDomainData(versions.latest, true, true);
+
+                        // load layer version & latest ATT&CK version
+                        let dataSubscription = forkJoin(loads).subscribe({
+                            next: (res) => {
+                                // TODO: diff_stix stuff
+                                this.dataService.compareVersions(versions.previous, versions.latest);
+                                viewModel.deSerialize(string);
+                                viewModel.loadVMData();
+                            },
+                            complete: () => { dataSubscription.unsubscribe(); }
+                        });
+                    }
+                    else if (!this.dataService.getDomain(viewModel.domainID).dataLoaded) {
                         this.dataService.loadDomainData(viewModel.domainID, true).then( () => {
                             viewModel.deSerialize(string);
                             viewModel.loadVMData();
