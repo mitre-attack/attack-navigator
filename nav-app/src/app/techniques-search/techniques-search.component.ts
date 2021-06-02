154f9942e246d94d6cba257f91aac8ee1b27b4d8
Anna Lin <alin@mitre.org>
2021-06-02 16:19:15 -0400

    Combined search and multi-select as accordion expansion panels in sidebar; Updated search feature to query StixObjects also--the search queries the same decreasing number of StixObjects until either: 1) there's no results, or 2) the query clears out (is an empty string ""); it was too slow to repeat the search with a new set of StixObjects each time, so there's getStixData() and getStixResults()

--- nav-app/src/app/techniques-search/techniques-search.component.ts
+++ nav-app/src/app/techniques-search/techniques-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ViewModel } from '../viewmodels.service';
-import { DataService, Technique } from '../data.service';
+import { MatDialog } from '@angular/material/dialog';
+import {BaseStix, DataService, Group, Mitigation, Software, Technique} from '../data.service';
 
 @Component({
   selector: 'app-techniques-search',
@@ -9,6 +10,8 @@ import { DataService, Technique } from '../data.service';
 })
 export class TechniquesSearchComponent implements OnInit {
     @Input() viewModel: ViewModel;
+    public stixTypes: any[];
+
     public fields = [
         {
             "label": "name",
@@ -39,18 +42,33 @@ export class TechniquesSearchComponent implements OnInit {
 
     private _query: string = "";
     public set query(newQuery: string) {
-        let self = this;
         this._query = newQuery;
-        if (this._query.trim() != "") {
-            // search
+        this.getStixResults(this._query);
+        this.getTechniqueResults(this._query)
+    }
+    public get queryLength(): number {
+        return this._query.length;
+    }
+    public techniqueResults: Technique[] = [];
 
+    constructor(private dataService: DataService) {
+        this.stixTypes = [];
+    }
+
+    ngOnInit() {
+        this.getStixData()
+    }
+
+    getTechniqueResults(query= "") {
+        let self = this;
+        if (query.trim() != "") {
             //get master list of techniques and sub-techniques
             let allTechniques = this.dataService.getDomain(this.viewModel.domainID).techniques;
             for (let technique of allTechniques) {
                 allTechniques = allTechniques.concat(technique.subtechniques);
             }
 
-            let results = allTechniques.filter(function(technique: Technique) {
+            let techniqueResults = allTechniques.filter(function(technique: Technique) {
                 for (let field of self.fields) {
                     if (field.enabled) {
                         // query in this field
@@ -61,7 +79,7 @@ export class TechniquesSearchComponent implements OnInit {
             });
             // deconflict IDs for cross-tactic techniques
             let seenIDs = new Set();
-            results = results.filter(function(technique: Technique) {
+            techniqueResults = techniqueResults.filter(function(technique: Technique) {
                 if (seenIDs.has(technique.id)) return false;
                 else {
                     seenIDs.add(technique.id);
@@ -69,27 +87,47 @@ export class TechniquesSearchComponent implements OnInit {
                 }
             })
 
-            results = results.sort((tA: Technique, tB: Technique) => {
+            techniqueResults = techniqueResults.sort((tA: Technique, tB: Technique) => {
                 let c1 = tA.isSubtechnique ? tA.parent.name : tA.name;
                 let c2 = tB.isSubtechnique ? tB.parent.name : tB.name;
                 return c1.localeCompare(c2)
             });
-            // console.log(seenIDs, results)
-            this.results = results;
+            // console.log(seenIDs, techniqueResults)
+            this.techniqueResults = techniqueResults;
         } else {
-            this.results = [];
+            this.techniqueResults = [];
         }
     }
-    public get queryLength(): number {
-        return this._query.length;
+
+    getStixData() {
+        let domain = this.dataService.getDomain(this.viewModel.domainID);
+
+        this.stixTypes = [{
+            "label": "threat groups",
+            "objects": this.filterAndSort(domain.groups.filter((group, i, arr) => arr.findIndex(t => t.id === group.id) === i))
+        }, {
+            "label": "software",
+            "objects": this.filterAndSort(domain.software)
+        }, {
+            "label": "mitigations",
+            "objects": this.filterAndSort(domain.mitigations)
+        }]
     }
-    public results: Technique[] = [];
 
-    constructor(private dataService: DataService) {
-
+    getStixResults(query= "") {
+        if (query.trim() != "") {
+            this.stixTypes.forEach(item => item['objects'] = this.filterAndSort(item['objects'], query));
+        } else {
+            this.getStixData();
+        }
     }
 
-    ngOnInit() {
+    public filterAndSort(items: any[], query= "") {
+        let results = items;
+        if (query.trim() != "") {
+            results = results.filter(item => item.name.toLowerCase().includes(query.trim().toLowerCase()))
+        }
+        return results.sort((a,b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
     }
 
     public toggleFieldEnabled(field: string) {
@@ -111,11 +149,37 @@ export class TechniquesSearchComponent implements OnInit {
     }
 
     public selectAll(): void {
-        for (let result of this.results) this.select(result);
+        for (let result of this.techniqueResults) this.select(result);
     }
 
     public deselectAll(): void {
-        for (let result of this.results) this.deselect(result);
+        for (let result of this.techniqueResults) this.deselect(result);
     }
 
+    public getRelated(stixObject: BaseStix): Technique[] {
+        // master list of all techniques and sub-techniques
+        let techniques = this.dataService.getDomain(this.viewModel.domainID).techniques;
+        let allTechniques = techniques.concat(this.dataService.getDomain(this.viewModel.domainID).subtechniques);
+        let domainID = this.viewModel.domainID;
+
+        if (stixObject instanceof Group) {
+            return allTechniques.filter((technique: Technique) => (stixObject as Group).relatedTechniques(domainID).includes(technique.id));
+        } else if (stixObject instanceof Software) {
+            return allTechniques.filter((technique: Technique) => (stixObject as Software).relatedTechniques(domainID).includes(technique.id));
+        } else if (stixObject instanceof Mitigation) {
+            return allTechniques.filter((technique: Technique) => (stixObject as Mitigation).relatedTechniques(domainID).includes(technique.id));
+        }
+    }
+
+    public deselectStix(stixObject: BaseStix): void {
+        for (let technique of this.getRelated(stixObject)) {
+            this.viewModel.unselectTechniqueAcrossTactics(technique);
+        }
+    }
+
+    public selectStix(stixObject: BaseStix): void {
+        for (let technique of this.getRelated(stixObject)) {
+            this.viewModel.selectTechniqueAcrossTactics(technique);
+        }
+    }
 }
