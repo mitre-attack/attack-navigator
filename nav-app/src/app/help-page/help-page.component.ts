78866770c541823cbe0e18831dd46092c66a6060
Anna Lin <alin@mitre.org>
2021-06-30 11:13:21 -0400

    Styling help.md

--- nav-app/src/app/help-page/help-page.component.ts
+++ nav-app/src/app/help-page/help-page.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MarkdownComponent } from 'ngx-markdown';
 
 @Component({
   selector: 'help-page',
   templateUrl: './help-page.component.html',
-  styleUrls: ['./help-page.component.scss']
+  styleUrls: ['./help-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HelpPageComponent implements OnInit {
   @ViewChild('markdownElement', {static: false}) private markdownElement: MarkdownComponent;
--- nav-app/src/app/help/help.component.ts
+++ nav-app/src/app/help/help.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewEncapsulation } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import * as globals from "../globals";
 
 @Component({
     selector: 'help',
     templateUrl: './help.component.html',
-    styleUrls: ['./help.component.scss']
+    styleUrls: ['./help.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class HelpComponent implements OnInit {
     nav_version: string = globals.nav_version;
 
-    constructor(private dialogRef: MatDialogRef<HelpComponent>, 
+    constructor(private dialogRef: MatDialogRef<HelpComponent>,
                 @Inject(MAT_DIALOG_DATA) public data) { }
 
     ngOnInit() { }
