56061df7c8ee8fec81b11fbeeeabc371a87e2f8d
Anna Lin <alin@mitre.org>
2021-08-12 15:40:57 -0400

    Added case to check external_id exists too, updated error thrown

--- nav-app/src/app/data.service.ts
+++ nav-app/src/app/data.service.ts
@@ -39,7 +39,8 @@ export class DataService {
 
     /**
      * Parse the given stix bundle into the relevant data holders
-     * @param {any[]} stixBundle: the STIX bundle to parse
+     * @param domain
+     * @param stixBundles
      */
     parseBundle(domain: Domain, stixBundles: any[]): void {
         let platforms = new Set<String>();
@@ -327,9 +328,9 @@ export abstract class BaseStix {
         this.id = stixSDO.id;
         this.name = stixSDO.name;
         this.description = stixSDO.description;
-        if (stixSDO.external_references && stixSDO.external_references[0]) this.attackID = stixSDO.external_references[0].external_id; else {
-          alert('Error: external_id in external_references not found in imported BaseStix object id (' + stixSDO.id + ')');
-          throw console.error('Error: external_id in external_references not found in imported BaseStix object', stixSDO, ' Read more here: https://docs.oasis-open.org/cti/stix/v2.1/os/stix-v2.1-os.html#_72bcfr3t79jx');
+        if (stixSDO.external_references && stixSDO.external_references[0] && stixSDO.external_references[0].external_id) this.attackID = stixSDO.external_references[0].external_id; else {
+          alert('Error: external_references has invalid format in imported BaseStix object (ID: ' + stixSDO.id + ')');
+          throw new Error('Error: external_references has invalid format in imported BaseStix object. Read more here: https://docs.oasis-open.org/cti/stix/v2.1/os/stix-v2.1-os.html#_72bcfr3t79jx');
         }
         this.url = stixSDO.external_references[0].url;
         this.dataService = dataService;
