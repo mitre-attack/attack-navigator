58994f861eaebda18dedc29e30f4f78a0dd40999
Anna Lin <alin@mitre.org>
2021-10-18 16:12:40 -0400

    Updated dark theme disabled cell color

--- nav-app/src/app/matrix/cell.ts
+++ nav-app/src/app/matrix/cell.ts
@@ -75,7 +75,7 @@ export abstract class Cell {
         let isDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
         if (!this.tactic) return isDarkTheme ? "white" : "black";
         let tvm = this.viewModel.getTechniqueVM(this.technique, this.tactic)
-        if (!tvm.enabled) return isDarkTheme ? "white" : "#aaaaaa";
+        if (!tvm.enabled) return isDarkTheme ? "rgb(255 255 255 / 25%)" : "#aaaaaa";
         // don't display if disabled or highlighted
         // if (this.viewModel.highlightedTechnique && this.viewModel.highlightedTechnique.technique_tactic_union_id == this.technique.technique_tactic_union_id) return "black"
         if (tvm.color) return tinycolor.mostReadable(tvm.color, ["white", "black"]);
