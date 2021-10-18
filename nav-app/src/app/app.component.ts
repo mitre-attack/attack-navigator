a107f11a0a5e1a268055eb03ba3ce67a20440077
Anna Lin <alin@mitre.org>
2021-10-18 13:08:38 -0400

    Icons overhaul of controls toolbar--no longer using <img> or <svg> to get icons, now using <span class="material-icons"> or <mat-icon svgIcon=""> for (custom icons) instead; fixed Legend bar

--- nav-app/src/app/app.component.ts
+++ nav-app/src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component, ViewChild, DoCheck, HostListener } from '@angular/core';
+import { Component, ViewChild, DoCheck, HostListener, OnInit } from '@angular/core';
 import { TabsComponent } from './tabs/tabs.component';
 import { ConfigService } from './config.service';
 import * as globals from "./globals";
+import { IconsService } from "./icons.service";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     @ViewChild(TabsComponent) tabsComponent;
 
     nav_version: string = globals.nav_version;
@@ -22,7 +23,7 @@ export class AppComponent {
         $event.returnValue='Are you sure you want to navigate away? Your data may be lost!';
     }
 
-    constructor(public configService: ConfigService) {
+    constructor(public configService: ConfigService, private iconsService: IconsService) {
         Array.prototype.includes = function(value): boolean {
             // console.log("checking include")
             for (let i = 0; i < this.length; i++) {
@@ -32,6 +33,10 @@ export class AppComponent {
         }
         window.onload = () => this.matcher();
         window.matchMedia("(prefers-color-scheme: dark)").addEventListener('change', () => this.matcher());
+    }
+
+    ngOnInit() {
+        this.iconsService.registerIcons();
     }
 
     matcher() {
