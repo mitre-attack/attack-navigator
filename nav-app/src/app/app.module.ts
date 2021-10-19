7f43fab215ec6c61fe5f18934d10092163061ac2
Isabel Tuson <isabel@mitre.org>
2021-10-19 14:11:56 -0400

    fix formatting weirdness, hopefully

--- nav-app/src/app/app.module.ts
+++ nav-app/src/app/app.module.ts
@@ -1,52 +1,54 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from "@angular/core";
-import "rxjs/add/operator/map";
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import 'rxjs/add/operator/map'
 
 // material
-import { MatInputModule } from "@angular/material/input";
-import { MatSelectModule } from "@angular/material/select";
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { ColorPickerModule } from "ngx-color-picker";
-import { DndModule } from "ngx-drag-drop";
-import { PopoverModule } from "ngx-smart-popover";
-import { HttpClientModule } from "@angular/common/http";
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { DndModule } from 'ngx-drag-drop';
+import { PopoverModule } from 'ngx-smart-popover';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from "./app.component";
-import { DataTableComponent } from "./datatable/data-table.component";
-import { TabsComponent } from "./tabs/tabs.component";
-import { HelpComponent } from "./help/help.component";
-import { ExporterComponent } from "./exporter/exporter.component";
-import { TechniqueCellComponent } from "./matrix/technique-cell/technique-cell.component";
-import { MatrixSideComponent } from "./matrix/matrix-side/matrix-side.component";
-import { MatrixFlatComponent } from "./matrix//matrix-flat/matrix-flat.component";
-import { MatrixMiniComponent } from "./matrix//matrix-mini/matrix-mini.component";
-import { TooltipComponent } from "./matrix/technique-cell/tooltip/tooltip.component";
-import { SearchAndMultiselectComponent } from "./search-and-multiselect/search-and-multiselect.component";
-import { ContextmenuComponent } from "./matrix/technique-cell/contextmenu/contextmenu.component";
-import { TacticCellComponent } from "./matrix/tactic-cell/tactic-cell.component";
-import { VersionUpgradeComponent } from "./version-upgrade/version-upgrade.component";
-import { SidebarComponent } from "./sidebar/sidebar.component";
-import { LayerUpgradeComponent } from "./layer-upgrade/layer-upgrade.component";
-import { ChangelogCellComponent } from "./layer-upgrade/changelog-cell/changelog-cell.component";
+import { AppComponent } from './app.component';
+import { DataTableComponent } from './datatable/data-table.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { HelpComponent } from './help/help.component';
+import { ExporterComponent } from './exporter/exporter.component';
+import { TechniqueCellComponent } from './matrix/technique-cell/technique-cell.component';
+import { MatrixSideComponent } from './matrix/matrix-side/matrix-side.component';
+import { MatrixFlatComponent } from './matrix//matrix-flat/matrix-flat.component';
+import { MatrixMiniComponent } from './matrix//matrix-mini/matrix-mini.component';
+import { TooltipComponent } from './matrix/technique-cell/tooltip/tooltip.component';
+import { SearchAndMultiselectComponent } from './search-and-multiselect/search-and-multiselect.component';
+import { ContextmenuComponent } from './matrix/technique-cell/contextmenu/contextmenu.component';
+import { TacticCellComponent } from './matrix/tactic-cell/tactic-cell.component';
+import { VersionUpgradeComponent } from './version-upgrade/version-upgrade.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LayerUpgradeComponent } from './layer-upgrade/layer-upgrade.component';
+import { ChangelogCellComponent } from './layer-upgrade/changelog-cell/changelog-cell.component';
 import { MatCardModule } from "@angular/material/card";
 import { MatDividerModule } from "@angular/material/divider";
-import { MatStepperModule } from "@angular/material/stepper";
-import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
 
 import { MarkdownModule } from "ngx-markdown";
-import { LayerInformationComponent } from "./layer-information/layer-information.component";
-import { ChangelogComponent } from "./changelog/changelog.component";
+import { LayerInformationComponent } from './layer-information/layer-information.component';
+import { ChangelogComponent } from './changelog/changelog.component';
 import { MatTabsModule } from "@angular/material/tabs";
+
 
 @NgModule({
   declarations: [
@@ -95,20 +97,20 @@ import { MatTabsModule } from "@angular/material/tabs";
     MatPaginatorModule,
     MarkdownModule.forRoot(),
     PopoverModule,
-    MatTabsModule,
+    MatTabsModule
   ],
   exports: [
-    MatSelectModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatTooltipModule,
-    MatMenuModule,
-    MatExpansionModule,
-    MatTabsModule,
+      MatSelectModule,
+      MatInputModule,
+      MatButtonModule,
+      MatIconModule,
+      MatTooltipModule,
+      MatMenuModule,
+      MatExpansionModule,
+      MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents: [VersionUpgradeComponent, HelpComponent, ExporterComponent],
+  entryComponents: [ VersionUpgradeComponent, HelpComponent, ExporterComponent ]
 })
-export class AppModule {}
+export class AppModule { }
