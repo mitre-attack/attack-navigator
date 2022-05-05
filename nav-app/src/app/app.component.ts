c3ebc83f1ce9c5e3bec4c8fa7d3dba5206055ff6
Anna Lin <alin@mitre.org>
2022-05-05 17:05:20 -0400

    Try to trigger build failure in github actions

--- nav-app/src/app/app.component.ts
+++ nav-app/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild, HostListener, OnInit } from '@angular/core';
-import { TabsComponent } from './tabs/tabs.component';
-import { ConfigService } from './config.service';
+
 import * as globals from "./globals";
 import { IconsService } from "./icons.service";
 import { deleteCookie, getCookie, hasCookie, setCookie } from "./cookies";
