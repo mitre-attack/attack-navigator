149da45e57e9e14b13bb788001004f466ec467ef
Jared Ondricek <jondricek@mitre.org>
2022-07-31 17:05:27 -0500

    Fix typo in test name

--- nav-app/src/app/app.component.spec.ts
+++ nav-app/src/app/app.component.spec.ts
@@ -18,7 +18,7 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'ATT&CK®; Navigator'`, waitForAsync(() => {
+  it(`should have as title 'ATT&CK® Navigator'`, waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('ATT&CK® Navigator');
