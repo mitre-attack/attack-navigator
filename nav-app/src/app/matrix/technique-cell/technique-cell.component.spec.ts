a1e252b90ef2fd972919af261002cec139e712d2
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-08-03 15:48:06 -0400

    Enable Jasmine unit test that validates that an instance of TechniqueCellComponent can be created using the Angular dependency injection system

--- nav-app/src/app/matrix/technique-cell/technique-cell.component.spec.ts
+++ nav-app/src/app/matrix/technique-cell/technique-cell.component.spec.ts
@@ -1,66 +1,20 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 import { TechniqueCellComponent } from './technique-cell.component';
-import { Matrix, Technique } from '../../data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('TechniqueCellComponent', () => {
   let component: TechniqueCellComponent;
-  let fixture: ComponentFixture<TechniqueCellComponent>;
-  let matrixStub: Partial<Matrix>;
-  let techniqueStub: Partial<Technique>;
 
-  beforeEach(waitForAsync(() => {
-
-    matrixStub = {}
-    techniqueStub = {
-      id: "1111-2222-3333-4444",
-      attackID: "T1234",
-    }
-
-    TestBed.configureTestingModule({
-    imports: [ HttpClientTestingModule ],
-      declarations: [ TechniqueCellComponent ],
-      providers: [ { provide: Technique, useValue: techniqueStub } ]
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TechniqueCellComponent);
-    component = fixture.componentInstance;
-
-    // let test_technique = new Technique({id: "1111-2222-1111"}, null, null)
-    // let test_technique = new Technique(0, null, null)
-    // component.technique = test_technique
-
-    // component.technique = {
-    //   id: "1111-2222-3333-4444",
-    //   attackID: "T1234",
-    //   name: "This is a technique",
-    //   description: "some description",
-    //   url: "something",
-    //   created: "2020",
-    //   modified: "2022",
-    //   revoked: false,
-    //   deprecated: false,
-    //   version: "string",
-    //   platforms: ["enterprise"],
-    //   tactics: ["collection"],
-    //   subtechniques: [],
-    //   datasources: "DATA SOURCES!! YAY!",
-    //   parent: null,
-    //   isSubtechnique: false,
-    //   get_technique_tactic_id: null,
-    //   get_all_technique_tactic_ids: null,
-    //   dataService: null,
-    //   compareVersion: null,
-    //   revoked_by: null,
-    // }
-
-    fixture.detectChanges();
+  beforeAll(() => {
+    // Instantiate a dummy Angular Module and use DI to initialize the TechniqueCellComponent
+    const moduleRef = TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [TechniqueCellComponent]
+    });
+    component = moduleRef.inject<TechniqueCellComponent>(TechniqueCellComponent);
   });
 
-  it('should create', () => {
-    console.log(component)
-    expect(component).toBeTruthy();
+  it('can create an instance of TechniqueCellComponent', () => {
+    expect(component).toBeDefined();
   });
 });
