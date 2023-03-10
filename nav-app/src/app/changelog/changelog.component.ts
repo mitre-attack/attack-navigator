0facd882f7a3257c5ebb644eb9984e31609e0194
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2023-03-10 11:00:43 -0500

    changelog and help cleanup

--- nav-app/src/app/changelog/changelog.component.ts
+++ nav-app/src/app/changelog/changelog.component.ts
@@ -1,20 +1,18 @@
-import { Component, Inject, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from "@angular/material/dialog";
 import { MarkdownComponent, MarkdownService } from "ngx-markdown";
 
 @Component({
-  selector: 'app-changelog',
-  templateUrl: './changelog.component.html',
-  styleUrls: ['./changelog.component.scss']
+    selector: 'app-changelog',
+    templateUrl: './changelog.component.html',
+    styleUrls: ['./changelog.component.scss']
 })
-export class ChangelogComponent implements OnInit {
-  @ViewChild('markdownElement', {static: false}) private markdownElement: MarkdownComponent;
+export class ChangelogComponent {
+    @ViewChild('markdownElement', { static: false }) private markdownElement: MarkdownComponent;
 
-  constructor(private dialog: MatDialog,
-              private markdownService: MarkdownService,
-              @Inject(MAT_DIALOG_DATA) public data) {}
-
-  ngOnInit(): void {
-  }
-
+    constructor(private dialog: MatDialog,
+                private markdownService: MarkdownService,
+                @Inject(MAT_DIALOG_DATA) public data) {
+        // intentionally left blank
+    }
 }
