015b4e6b7ef139a8edbcd40b9483bc4388c85e11
adpare <apare@mitre.org>
2023-10-27 14:26:13 -0400

    viewmodels service test

--- nav-app/src/app/services/viewmodels.service.spec.ts
+++ nav-app/src/app/services/viewmodels.service.spec.ts
@@ -11,7 +11,39 @@ describe('ViewmodelsService', () => {
         });
     });
 
-    it('should be created', inject([ViewModelsService], (service: ViewModelsService) => {
-        expect(service).toBeTruthy();
-    }));
+	it('should be created', inject([ViewModelsService], (service: ViewModelsService) => {
+		expect(service).toBeTruthy();
+	}));
+
+	it('selectionChanged should be defined', inject([ViewModelsService], (service: ViewModelsService) => {
+		expect(service.selectionChanged).toBeDefined();
+	}));
+
+	it('viewmodel by inheriting opsettings created', inject([ViewModelsService], (service: ViewModelsService) => {
+		let opSettings: any = {
+			domain: "Enterprise v13",
+			gradientVM: null,
+			coloringVM: null,
+			commentVM: null,
+			linkVM: null,
+			metadataVM: null,
+			enabledVM: null,
+			filterVM: null,
+			scoreExpression: "",
+			legendVM: null
+		}
+		let vm = service.layerOperation(null, "test1", opSettings);
+		expect(vm.domainVersionID).toBe("Enterprise v13");
+	}));
+
+	it('viewmodel is created', inject([ViewModelsService], (service: ViewModelsService) => {
+		let vm = service.newViewModel("test","test");
+		expect(service.viewModels.length).toBe(1);
+	}));
+
+	it('viewmodel is destroyed', inject([ViewModelsService], (service: ViewModelsService) => {
+		let vm = service.newViewModel("test","test");
+		service.destroyViewModel(vm);
+		expect(service.viewModels.length).toBe(0);
+	}));
 });
