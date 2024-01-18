05ce8275890489b8804677618c8d57a1b161fdd4
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2024-01-18 14:26:06 -0500

    added technique vm tests

--- nav-app/src/app/services/viewmodels.service.spec.ts
+++ nav-app/src/app/services/viewmodels.service.spec.ts
@@ -134,4 +134,100 @@ describe('ViewmodelsService', () => {
 		service.destroyViewModel(vm);
 		expect(service.viewModels.length).toBe(0);
 	}));
+
+    const validRep = '{"comment": "test comment","color": "#ffffff", "score": 1,"enabled": true,"showSubtechniques": false}';
+    const techniqueID = 'T0001';
+    const tacticName = 'tactic-name';
+    const ttid = 'T0001^tactic-name'
+    it('should deserialize TechniqueVM correctly with valid input', () => {
+        let tvm = new TechniqueVM(ttid);
+        let deserializeSpy = spyOn(TechniqueVM.prototype, 'deserialize').and.callThrough();
+        tvm.deserialize(validRep, techniqueID, tacticName);
+        expect(deserializeSpy).toHaveBeenCalledOnceWith(validRep, techniqueID, tacticName);
+        expect(tvm.techniqueID).toBe(techniqueID);
+        expect(tvm.tactic).toBe(tacticName);
+        expect(tvm.comment).toBe("test comment");
+        expect(tvm.color).toBe("#ffffff");
+        expect(tvm.score).toBe('1');
+        expect(tvm.enabled).toBe(true);
+        expect(tvm.showSubtechniques).toBe(false);
+    });
+
+    it('should handle missing techniqueID field', () => {
+        let tvm = new TechniqueVM(ttid);
+        spyOn(console, 'error');
+        spyOn(TechniqueVM.prototype, 'deserialize').and.callThrough();
+        tvm.deserialize(validRep, undefined, tacticName);
+        expect(console.error).toHaveBeenCalled();
+    });
+
+    it('should handle missing tactic field', () => {
+        let tvm = new TechniqueVM(ttid);
+        spyOn(console, 'error');
+        spyOn(window, 'alert');
+        spyOn(TechniqueVM.prototype, 'deserialize').and.callThrough();
+        tvm.deserialize(validRep, techniqueID, undefined);
+        expect(console.error).toHaveBeenCalled();
+        expect(window.alert).toHaveBeenCalled();
+    });
+
+    it('should handle missing techniqueID and tactic field', () => {
+        let tvm = new TechniqueVM(ttid);
+        tvm.techniqueID = undefined;
+        tvm.tactic = undefined;
+        spyOn(console, 'error');
+        spyOn(TechniqueVM.prototype, 'deserialize').and.callThrough();
+        tvm.deserialize(validRep, undefined, undefined);
+        expect(console.error).toHaveBeenCalledTimes(3);
+    });
+
+    it('should handle non-string comment field', () => {
+        let tvm = new TechniqueVM(ttid);
+        spyOn(console, 'error');
+        spyOn(TechniqueVM.prototype, 'deserialize').and.callThrough();
+        const invalidRep = '{"comment": 10,"color": "#ffffff", "score": 1,"enabled": true,"showSubtechniques": false}';
+        tvm.deserialize(invalidRep, techniqueID, tacticName);
+        expect(tvm.comment).toBe('');
+        expect(console.error).toHaveBeenCalled();
+    });
+
+    it('should handle non-string color field', () => {
+        let tvm = new TechniqueVM(ttid);
+        spyOn(console, 'error');
+        spyOn(TechniqueVM.prototype, 'deserialize').and.callThrough();
+        const invalidRep = '{"comment": "test comment","color": 10, "score": 1,"enabled": true,"showSubtechniques": false}';
+        tvm.deserialize(invalidRep, techniqueID, tacticName);
+        expect(tvm.color).toBe('');
+        expect(console.error).toHaveBeenCalled();
+    });
+
+    it('should handle non-number score field', () => {
+        let tvm = new TechniqueVM(ttid);
+        spyOn(console, 'error');
+        spyOn(TechniqueVM.prototype, 'deserialize').and.callThrough();
+        const invalidRep = '{"comment": "test comment","color": "#ffffff", "score": "1","enabled": true,"showSubtechniques": false}';
+        tvm.deserialize(invalidRep, techniqueID, tacticName);
+        expect(tvm.score).toBe('');
+        expect(console.error).toHaveBeenCalled();
+    });
+
+    it('should handle non-boolean enabled field', () => {
+        let tvm = new TechniqueVM(ttid);
+        spyOn(console, 'error');
+        spyOn(TechniqueVM.prototype, 'deserialize').and.callThrough();
+        const invalidRep = '{"comment": "test comment","color": "#ffffff", "score": 1,"enabled": "true","showSubtechniques": false}';
+        tvm.deserialize(invalidRep, techniqueID, tacticName);
+        expect(tvm.enabled).toBe(true);
+        expect(console.error).toHaveBeenCalled();
+    });
+
+    it('should handle non-boolean showSubtechniques field', () => {
+        let tvm = new TechniqueVM(ttid);
+        spyOn(console, 'error');
+        spyOn(TechniqueVM.prototype, 'deserialize').and.callThrough();
+        const invalidRep = '{"comment": "test comment","color": "#ffffff", "score": 1,"enabled": true,"showSubtechniques": "false"}';
+        tvm.deserialize(invalidRep, techniqueID, tacticName);
+        expect(tvm.showSubtechniques).toBe(false);
+        expect(console.error).toHaveBeenCalled();
+    });
 });
