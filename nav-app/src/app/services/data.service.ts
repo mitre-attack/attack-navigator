f0e47cde2bac0130e239551de7681e7311859759
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2024-01-26 15:28:48 -0500

    more small changes

--- nav-app/src/app/services/data.service.ts
+++ nav-app/src/app/services/data.service.ts
@@ -236,11 +236,11 @@ export class DataService {
     private domainData$: Observable<Object>;
 
     // URLs in case config file doesn't load properly
-    private latestVersion: Version = { name: "ATT&CK v14", number: "14" };
-    private lowestSupportedVersion: Version; // used by tabs component
-    private enterpriseAttackURL: string = "https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json";
-    private mobileAttackURL: string = "https://raw.githubusercontent.com/mitre/cti/master/mobile-attack/mobile-attack.json";
-    private icsAttackURL: string = "https://raw.githubusercontent.com/mitre/cti/master/ics-attack/ics-attack.json";
+    public latestVersion: Version = { name: "ATT&CK v14", number: "14" };
+    public lowestSupportedVersion: Version; // used by tabs component
+    public enterpriseAttackURL: string = "https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json";
+    public mobileAttackURL: string = "https://raw.githubusercontent.com/mitre/cti/master/mobile-attack/mobile-attack.json";
+    public icsAttackURL: string = "https://raw.githubusercontent.com/mitre/cti/master/ics-attack/ics-attack.json";
 
     /**
      * Set up the URLs for data
