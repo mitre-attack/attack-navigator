1c7b06d3e523df88d337969265c379fdcad2aa99
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2024-02-03 11:57:54 -0500

    refactor technique list

--- nav-app/src/app/matrix/matrix-common.spec.ts
+++ nav-app/src/app/matrix/matrix-common.spec.ts
@@ -8,6 +8,10 @@ import * as MockData from '../../tests/utils/mock-data';
 
 describe('MatrixCommon', () => {
     let matrixCommon: MatrixCommon;
+	let techniqueList: Technique[];
+	let technique1: Technique;
+	let technique2: Technique;
+	let idToTacticSDO = new Map<string, any>();
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -16,6 +20,14 @@ describe('MatrixCommon', () => {
         });
         matrixCommon = TestBed.inject(MatrixCommon);
 		matrixCommon.viewModel = new ViewModel("layer","1","enterprise-attack-13",null);
+		idToTacticSDO.set("tactic-0", MockData.TA0000);
+
+		// create technique list
+		let subtechnique1 = new Technique(MockData.T0000_002,[],null);
+        let subtechnique2 = new Technique(MockData.T0000_000,[],null);
+		technique1 = new Technique(MockData.T0000,[subtechnique1, subtechnique2],null);
+		technique2 = new Technique(MockData.T0001,[],null);
+		techniqueList = [technique1, technique2];
     });
 
     it('should be created', () => {
@@ -31,17 +43,8 @@ describe('MatrixCommon', () => {
     });
 
     it('should filter techniques and tactics', () => {
-        let technique_list: Technique[] = [];
         let tactic_list: Tactic[] = [];
-        let idToTacticSDO = new Map<string, any>();
-        idToTacticSDO.set("tactic-0", MockData.TA0000);
         matrixCommon.viewModel.showTacticRowBackground = true;
-        let st1 = new Technique(MockData.T0000_002,[],null);
-        let st2 = new Technique(MockData.T0000_000,[],null);
-        let t1 = new Technique(MockData.T0000,[st1, st2],null);
-        technique_list.push(t1);
-        let t2 = new Technique(MockData.T0001,[],null);
-        technique_list.push(t2);
         let tvm_1 = new TechniqueVM("T0000^tactic-name");
         let stvm_1 = new TechniqueVM("T0000.002^tactic-name");
         let stvm_2 = new TechniqueVM("T0000.000^tactic-name");
@@ -57,34 +60,25 @@ describe('MatrixCommon', () => {
         matrixCommon.viewModel.layout.showAggregateScores = true;
         matrixCommon.viewModel.layout.aggregateFunction = "min";
         matrixCommon.viewModel.filters.platforms.selection = ["PRE"];
-        let tactic1 = new Tactic(MockData.TA0000,technique_list,null);
+        let tactic1 = new Tactic(MockData.TA0000,techniqueList,null);
         tactic_list.push(tactic1);
-        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,technique_list,null);
-        expect(matrixCommon.filterTechniques(technique_list, tactic1)).toEqual([t1, t2]);
+        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,techniqueList,null);
+        expect(matrixCommon.filterTechniques(techniqueList, tactic1)).toEqual([technique1, technique2]);
         matrixCommon.viewModel.loaded = true;
         matrixCommon.filterTactics(tactic_list);
         expect(matrixCommon.filterTactics(tactic_list)).toEqual(tactic_list);
         matrixCommon.viewModel.hideDisabled = false;
-        expect(matrixCommon.filterTechniques(technique_list, tactic1)).toEqual([t1, t2]);
+        expect(matrixCommon.filterTechniques(techniqueList, tactic1)).toEqual([technique1, technique2]);
         MockData.matrixSDO.name = 'PRE-ATT&CK';
-        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,technique_list,null);
+        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,techniqueList,null);
         matrixCommon.viewModel.hideDisabled = true;
-        expect(matrixCommon.filterTechniques(technique_list, tactic1)).toEqual(technique_list);
+        expect(matrixCommon.filterTechniques(techniqueList, tactic1)).toEqual(techniqueList);
         spyOn(matrixCommon.viewModel, 'isSubtechniqueEnabled').and.returnValues(false);
-        expect(matrixCommon.filterTechniques(technique_list, tactic1)).toEqual([]);
+        expect(matrixCommon.filterTechniques(techniqueList, tactic1)).toEqual([]);
     });
 
     it('should sort techniques and tactics', () => {
-        let technique_list: Technique[] = [];
-        let idToTacticSDO = new Map<string, any>();
-        idToTacticSDO.set("tactic-0", MockData.TA0000);
         matrixCommon.viewModel.showTacticRowBackground = true;
-        let st1 = new Technique(MockData.T0000_002,[],null);
-        let st2 = new Technique(MockData.T0000_000,[],null);
-        let t1 = new Technique(MockData.T0000,[st1, st2],null);
-        technique_list.push(t1);
-        let t2 = new Technique(MockData.T0001,[],null);
-        technique_list.push(t2);
         let tvm_1 = new TechniqueVM("T0000^tactic-name");
         let stvm_1 = new TechniqueVM("T0000.002^tactic-name");
         let stvm_2 = new TechniqueVM("T0000.000^tactic-name");
@@ -100,67 +94,53 @@ describe('MatrixCommon', () => {
         matrixCommon.viewModel.layout.showAggregateScores = true;
         matrixCommon.viewModel.layout.aggregateFunction = "min";
         matrixCommon.viewModel.filters.platforms.selection = ["PRE"];
-        let tactic1 = new Tactic(MockData.TA0000,technique_list,null);
-        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,technique_list,null);
-        matrixCommon.applyControls(technique_list,tactic1);
-        matrixCommon.sortTechniques(technique_list,tactic1);
+        let tactic1 = new Tactic(MockData.TA0000,techniqueList,null);
+        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,techniqueList,null);
+        matrixCommon.applyControls(techniqueList,tactic1);
+        matrixCommon.sortTechniques(techniqueList,tactic1);
         matrixCommon.viewModel.layout.aggregateFunction = "max";
-        matrixCommon.applyControls(technique_list,tactic1);
-        matrixCommon.sortTechniques(technique_list,tactic1);
+        matrixCommon.applyControls(techniqueList,tactic1);
+        matrixCommon.sortTechniques(techniqueList,tactic1);
         matrixCommon.viewModel.layout.aggregateFunction = "sum";
-        matrixCommon.applyControls(technique_list,tactic1);
-        matrixCommon.sortTechniques(technique_list,tactic1);
+        matrixCommon.applyControls(techniqueList,tactic1);
+        matrixCommon.sortTechniques(techniqueList,tactic1);
         matrixCommon.viewModel.sorting = 1;
         matrixCommon.viewModel.layout.aggregateFunction = "average";
-        matrixCommon.applyControls(technique_list,tactic1);
-        matrixCommon.sortTechniques(technique_list,tactic1);
+        matrixCommon.applyControls(techniqueList,tactic1);
+        matrixCommon.sortTechniques(techniqueList,tactic1);
         matrixCommon.viewModel.sorting = 2;
         matrixCommon.viewModel.layout.aggregateFunction = "min";
-        matrixCommon.applyControls(technique_list,tactic1);
-        matrixCommon.sortTechniques(technique_list,tactic1);
+        matrixCommon.applyControls(techniqueList,tactic1);
+        matrixCommon.sortTechniques(techniqueList,tactic1);
         matrixCommon.viewModel.sorting = 3;
-        matrixCommon.sortTechniques(technique_list,tactic1);
+        matrixCommon.sortTechniques(techniqueList,tactic1);
         expect(matrixCommon).toBeTruthy();
     });
 
     it('should toggle', () => {
-        let technique_list: Technique[] = [];
-        let idToTacticSDO = new Map<string, any>();
-        idToTacticSDO.set("tactic-0", MockData.TA0000);
-        let st1 = new Technique(MockData.T0000_002,[],null);
-        let t1 = new Technique(MockData.T0000,[st1],null);
-        let t2 = new Technique(MockData.T0001,[],null);
-        technique_list.push(t1);
-        technique_list.push(t2);
         let tvm_1 = new TechniqueVM("T0000^tactic-name");
         let stvm_1 = new TechniqueVM("T0000.002^tactic-name");
         let tvm_2 = new TechniqueVM("T0001^tactic-name");
 		matrixCommon.viewModel.setTechniqueVM(tvm_1);
         matrixCommon.viewModel.setTechniqueVM(stvm_1);
         matrixCommon.viewModel.setTechniqueVM(tvm_2);
-        let tactic1 = new Tactic(MockData.TA0000,technique_list,null);
-        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,technique_list,null);
-        matrixCommon.onToggleSubtechniquesVisible(t1,tactic1);
-        let tvm = matrixCommon.viewModel.getTechniqueVM(t1, tactic1);
+        let tactic1 = new Tactic(MockData.TA0000,techniqueList,null);
+        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,techniqueList,null);
+        matrixCommon.onToggleSubtechniquesVisible(technique1,tactic1);
+        let tvm = matrixCommon.viewModel.getTechniqueVM(technique1, tactic1);
         expect(tvm.showSubtechniques).toEqual(true);
-        matrixCommon.onToggleSubtechniquesVisible(t2,tactic1);
+        matrixCommon.onToggleSubtechniquesVisible(technique2,tactic1);
         expect(tvm.showSubtechniques).toEqual(true);
     });
 
     it('should highlight and unhighlight technique', () => {
-        let technique_list: Technique[] = [];
-        let idToTacticSDO = new Map<string, any>();
-        idToTacticSDO.set("tactic-0", MockData.TA0000);
-        let st1 = new Technique(MockData.T0000_002,[],null);
-        let t1 = new Technique(MockData.T0000,[st1],null);
-        technique_list.push(t1);
         let tvm_1 = new TechniqueVM("T0000^tactic-name");
         let stvm_1 = new TechniqueVM("T0000.002^tactic-name");
 		matrixCommon.viewModel.setTechniqueVM(tvm_1);
         matrixCommon.viewModel.setTechniqueVM(stvm_1);
-        let tactic1 = new Tactic(MockData.TA0000,technique_list,null);
-        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,technique_list,null);
-        matrixCommon.onTechniqueHighlight(null,t1,tactic1);
+        let tactic1 = new Tactic(MockData.TA0000,techniqueList,null);
+        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,techniqueList,null);
+        matrixCommon.onTechniqueHighlight(null,technique1,tactic1);
         expect(matrixCommon.viewModel.highlightedTactic.name).toEqual("Name");
         matrixCommon.onTechniqueUnhighlight(null);
         expect(matrixCommon.viewModel.highlightedTactic).toEqual(null);
@@ -245,64 +225,49 @@ describe('MatrixCommon', () => {
     });
 
     it('should left click on technique', () => {
-        let technique_list: Technique[] = [];
-        let idToTacticSDO = new Map<string, any>();
-        idToTacticSDO.set("tactic-0", MockData.TA0000);
         let vm1 = matrixCommon.viewModelsService.newViewModel("test1","enterprise-attack-13");
-        let st1 = new Technique(MockData.T0000_002,[],null);
-        let t1 = new Technique(MockData.T0000,[st1],null);
-        technique_list.push(t1);
         let tvm_1 = new TechniqueVM("T0000^tactic-name");
         let stvm_1 = new TechniqueVM("T0000.002^tactic-name");
-        let t2 = new Technique(MockData.T0001,[],null);
-        technique_list.push(t2);
         let tvm_2 = new TechniqueVM("T0001^tactic-name");
         let stvm_2 = new TechniqueVM("T0001.002^tactic-name");
 		vm1.setTechniqueVM(tvm_1);
         vm1.setTechniqueVM(stvm_1);
         vm1.setTechniqueVM(tvm_2);
         vm1.setTechniqueVM(stvm_2);
-        let tactic1 = new Tactic(MockData.TA0000,technique_list,null);
-        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,technique_list,null);
+        let tactic1 = new Tactic(MockData.TA0000,techniqueList,null);
+        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,techniqueList,null);
         let e1 = new EventEmitter<any>();
         let event = {"shift":true};
         matrixCommon.viewModel = vm1;
         let emitSpy = spyOn(matrixCommon.viewModelsService.onSelectionChange, 'emit');
         matrixCommon.configService.setFeature("selecting_techniques", true);
-        matrixCommon.onTechniqueLeftClick(e1,t1,tactic1);
+        matrixCommon.onTechniqueLeftClick(e1,technique1,tactic1);
         expect(emitSpy).toHaveBeenCalled();
         expect(matrixCommon.viewModel.selectedTechniques.size).toEqual(1); // T0000
-        matrixCommon.onTechniqueLeftClick(event,t1,tactic1); 
+        matrixCommon.onTechniqueLeftClick(event,technique1,tactic1); 
         expect(emitSpy).toHaveBeenCalled();
         expect(matrixCommon.viewModel.selectedTechniques.size).toEqual(0);
-        matrixCommon.onTechniqueLeftClick(event,t1,tactic1);
+        matrixCommon.onTechniqueLeftClick(event,technique1,tactic1);
         expect(emitSpy).toHaveBeenCalled();
         expect(matrixCommon.viewModel.selectedTechniques.size).toEqual(1); // T0000
-        matrixCommon.onTechniqueLeftClick(e1,t1,tactic1);
+        matrixCommon.onTechniqueLeftClick(e1,technique1,tactic1);
         expect(emitSpy).toHaveBeenCalled();
         expect(matrixCommon.viewModel.selectedTechniques.size).toEqual(0);
         spyOn(matrixCommon.viewModel, 'isTechniqueSelected').and.returnValues(false);
         spyOn(matrixCommon.viewModel, 'getSelectedTechniqueCount').and.returnValue(2);
         vm1.activeTvm = tvm_2;
         matrixCommon.viewModel = vm1;
-        matrixCommon.onTechniqueLeftClick(e1,t1,tactic1);
+        matrixCommon.onTechniqueLeftClick(e1,technique1,tactic1);
         expect(emitSpy).toHaveBeenCalled();
     });
 
     it('on tactic click', () => {
-        let technique_list: Technique[] = [];
-        let idToTacticSDO = new Map<string, any>();
-        idToTacticSDO.set("tactic-0", MockData.TA0000);
-        let t1 = new Technique(MockData.T0000,[],null);
-        technique_list.push(t1);
         let tvm_1 = new TechniqueVM("T0000^tactic-name");
-        let t2 = new Technique(MockData.T0001,[],null);
-        technique_list.push(t2);
         let tvm_2 = new TechniqueVM("T0001^tactic-name");
 		matrixCommon.viewModel.setTechniqueVM(tvm_1);
         matrixCommon.viewModel.setTechniqueVM(tvm_2);
-        let tactic1 = new Tactic(MockData.TA0000,technique_list,null);
-        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,technique_list,null);
+        let tactic1 = new Tactic(MockData.TA0000,techniqueList,null);
+        matrixCommon.matrix = new Matrix(MockData.matrixSDO, idToTacticSDO,techniqueList,null);
         matrixCommon.onTacticClick(tactic1);
         expect(matrixCommon.viewModel.selectedTechniques.size).toEqual(2); // T0000, T0001
         spyOn(matrixCommon.viewModel, 'isTacticSelected').and.returnValue(true);
