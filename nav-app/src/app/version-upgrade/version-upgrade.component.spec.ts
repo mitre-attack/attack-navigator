380015c9cfb54ba4e3f58467ec8403c080b4addd
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2024-07-19 13:46:35 -0400

    version upgrade tests

--- nav-app/src/app/version-upgrade/version-upgrade.component.spec.ts
+++ nav-app/src/app/version-upgrade/version-upgrade.component.spec.ts
@@ -1,50 +1,50 @@
-// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { VersionUpgradeComponent } from './version-upgrade.component';
-// import { MatDialogModule, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-// describe('VersionUpgradeComponent', () => {
-//     let component: VersionUpgradeComponent;
-//     let fixture: ComponentFixture<VersionUpgradeComponent>;
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { VersionUpgradeComponent } from './version-upgrade.component';
+import { MatDialogModule, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+describe('VersionUpgradeComponent', () => {
+    let component: VersionUpgradeComponent;
+    let fixture: ComponentFixture<VersionUpgradeComponent>;
 
-//     beforeEach(waitForAsync(() => {
-//         TestBed.configureTestingModule({
-//             imports: [HttpClientTestingModule, MatDialogModule],
-//             declarations: [VersionUpgradeComponent],
-//             providers: [
-//                 {
-//                     provide: MatDialogRef,
-//                     useValue: {
-//                         close() {
-//                             return {};
-//                         },
-//                     },
-//                 },
-//                 {
-//                     provide: MAT_DIALOG_DATA,
-//                     useValue: {},
-//                 },
-//             ],
-//         }).compileComponents();
-//     }));
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, MatDialogModule],
+            declarations: [VersionUpgradeComponent],
+            providers: [
+                {
+                    provide: MatDialogRef,
+                    useValue: {
+                        close() {
+                            return {};
+                        },
+                    },
+                },
+                {
+                    provide: MAT_DIALOG_DATA,
+                    useValue: {},
+                },
+            ],
+        }).compileComponents();
+    }));
 
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(VersionUpgradeComponent);
-//         component = fixture.componentInstance;
-//         component.data.currVersion = '4.9';
-//         component.data.vmVersion = '4.8';
-//         component.data.layerName = 'test1';
-//         fixture.detectChanges();
-//     });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(VersionUpgradeComponent);
+        component = fixture.componentInstance;
+        component.data.currVersion = '4.9';
+        component.data.vmVersion = '4.8';
+        component.data.layerName = 'test1';
+        fixture.detectChanges();
+    });
 
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-//     it('should upgrade version', () => {
-//         const openDialogSpy = spyOn(component.dialogRef, 'close');
-//         component.upgradeVersion(true);
-//         expect(openDialogSpy).toHaveBeenCalledWith({
-//             upgrade: true,
-//         });
-//     });
-// });
+    it('should upgrade version', () => {
+        const openDialogSpy = spyOn(component.dialogRef, 'close');
+        component.upgradeVersion(true);
+        expect(openDialogSpy).toHaveBeenCalledWith({
+            upgrade: true,
+        });
+    });
+});
