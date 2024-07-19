5697a9338edcd807903ae42ee0f31dce9ebedae8
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2024-07-19 12:29:50 -0400

    sidebar tests

--- nav-app/src/app/sidebar/sidebar.component.spec.ts
+++ nav-app/src/app/sidebar/sidebar.component.spec.ts
@@ -1,30 +1,30 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { SidebarComponent } from './sidebar.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SidebarComponent } from './sidebar.component';
 
-// describe('SidebarComponent', () => {
-//     let component: SidebarComponent;
-//     let fixture: ComponentFixture<SidebarComponent>;
+describe('SidebarComponent', () => {
+    let component: SidebarComponent;
+    let fixture: ComponentFixture<SidebarComponent>;
 
-//     beforeEach(async () => {
-//         await TestBed.configureTestingModule({
-//             imports: [HttpClientTestingModule],
-//             declarations: [SidebarComponent],
-//         }).compileComponents();
-//     });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            declarations: [SidebarComponent],
+        }).compileComponents();
+    });
 
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(SidebarComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//     });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SidebarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-//     it('should set reloadToggle to false', () => {
-//         component.ngOnChanges();
-//         expect(component.reloadToggle).toEqual(false);
-//     });
-// });
+    it('should set reloadToggle to false', () => {
+        component.ngOnChanges();
+        expect(component.reloadToggle).toEqual(false);
+    });
+});
