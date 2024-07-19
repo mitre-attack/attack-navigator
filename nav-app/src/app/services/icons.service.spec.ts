5bafa1573c021eeb66e6227d9336a03045f6b921
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2024-07-19 12:21:27 -0400

    icon service tests

--- nav-app/src/app/services/icons.service.spec.ts
+++ nav-app/src/app/services/icons.service.spec.ts
@@ -1,22 +1,22 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { Icons, IconsService } from './icons.service';
+import { Icons, IconsService } from './icons.service';
 
-// describe('IconsService', () => {
-//     let service: IconsService;
+describe('IconsService', () => {
+    let service: IconsService;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({});
-//         service = TestBed.inject(IconsService);
-//     });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(IconsService);
+    });
 
-//     it('should be created', () => {
-//         expect(service).toBeTruthy();
-//     });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 
-//     it('should register', () => {
-//         spyOn(service.matIconRegistry, 'addSvgIcon');
-//         service.registerIcons();
-//         expect(service.matIconRegistry.addSvgIcon).toHaveBeenCalledTimes(Object.values(Icons).length);
-//     });
-// });
+    it('should register', () => {
+        spyOn(service.matIconRegistry, 'addSvgIcon');
+        service.registerIcons();
+        expect(service.matIconRegistry.addSvgIcon).toHaveBeenCalledTimes(Object.values(Icons).length);
+    });
+});
