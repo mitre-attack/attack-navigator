a8c91056a4dc3db23cd8273d5825ab002c8ae6ff
Charissa Miller <48832936+clemiller@users.noreply.github.com>
2024-07-24 09:06:45 -0400

    detect-browser

--- nav-app/src/app/utils/utils.ts
+++ nav-app/src/app/utils/utils.ts
@@ -1,4 +1,6 @@
 // utils.ts
+import { detect } from "detect-browser";
+
 let comparatorFn = {
 	'<': function(a, b) { return a < b; },
 	'<=': function(a, b) { return a <= b; },
@@ -15,9 +17,8 @@ export function isNumber(value: any): boolean {
 }
 
 export function isIE(): boolean {
-	let userAgent = (window.navigator && window.navigator.userAgent || '').toLowerCase();
-	let match = userAgent.match(/(?:msie |trident.+?; rv:)(\d+)/);
-	return match !== null;
+	const browser = detect();
+	return browser.name == 'ie';
 }
 
 export function isSafari(compRange): boolean {
@@ -28,7 +29,6 @@ export function isSafari(compRange): boolean {
 		return comparatorFn[op] ? comparatorFn[op](version, n) : (version == n || n !== n);
 	}
 
-	let userAgent = (window.navigator && window.navigator.userAgent || '').toLowerCase();
-	var match = userAgent.match(/version\/(\d+).+?safari/);
-	return match !== null && compare(match[1], compRange);
+	const browser = detect();
+	return browser.name == 'safari' && compare(browser.version.split('.')[0], compRange);
 }
